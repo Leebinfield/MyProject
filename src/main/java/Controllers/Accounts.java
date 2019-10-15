e84b68d46f6d75a6d751996734e6e8930bec1649
87436 <87436@farnborough.ac.uk>
2019-10-15 10:29:11 +0100

    Added methods into restaurant class /

--- src/main/java/Controllers/Accounts.java
+++ src/main/java/Controllers/Accounts.java
@@ -1,10 +1,48 @@
 package Controllers;
 
+import Server.Main;
+import com.sun.jersey.multipart.FormDataParam;
 import org.sqlite.SQLiteConfig;
+
+import java.awt.*;
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+import java.sql.PreparedStatement;
+import java.sql.*;
+import org.sqlite.SQLiteConfig;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+@Path("accounts/")
 public class Accounts {
-    public static void main(String[] args) {
-
-
+    //public static Connection db = null;
+    @POST
+    @Path("add")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String AddA(
+            @FormDataParam("id") Integer id, @FormDataParam("firstname") String firstname, @FormDataParam("lastname") String lastname){
+        try{
+            if (id == null || firstname == null || lastname == null ){
+                throw new Exception("One or more form data parameters are missing in http request.");
+            }
+            System.out.println("thing/new id=" +id);
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Accounts (id,firstname,lastname) VALUES (?,?,?)");
+            ps.setInt(1,id);
+            ps.setString(2,firstname);
+            ps.setString(3,lastname);
+            ps.execute();
+            return "{\"status\": \"OK\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to create new item, see server for more\"}";
+        }
     }
+
+
 }
--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -37,17 +37,16 @@ public class Main {
         }
 
 
-
-        Controllers.Restaurants.insertRest(2, "gu183ry" , 123);
-        Add();     //CREATE
-        Select();  //READ
-        Update();  //UPDATE
-        Delete();  //DESTROY
+        // Add();     //CREATE
+        // Select();  //READ
+        // Update();  //UPDATE
+        // Delete();  //DESTROY
         closeDatabase();
 
     }
+
     private static void openDatabase(String dbFile) {
-        try  {
+        try {
             Class.forName("org.sqlite.JDBC");
             SQLiteConfig config = new SQLiteConfig();
             config.enforceForeignKeys(true);
@@ -58,7 +57,8 @@ public class Main {
         }
 
     }
-    private static void closeDatabase(){
+
+    private static void closeDatabase() {
         try {
             db.close();
             System.out.println("Disconnected from database.");
@@ -66,61 +66,62 @@ public class Main {
             System.out.println("Database disconnection error: " + exception.getMessage());
         }
     }
-    public static void Select(){
-        try{
-            PreparedStatement ps = db.prepareStatement("Select AccName, FavFood from AccountName");
-            ResultSet results = ps.executeQuery();
-            while (results.next()){
-                String AccName = results.getString(1);
-                String Food = results.getString(2);
-                System.out.println("Username: " + AccName +" Favourite Food: " + Food);
-            }
-        }catch (Exception exception){
-            System.out.println("Database error: " + exception.getMessage());
-        }
-    }
-    public static void Add(){
-        try{
-            Scanner scanner = new Scanner(System.in);
-            String name = scanner.nextLine();
-            String fave = scanner.nextLine();
-            PreparedStatement ps = db.prepareStatement("INSERT INTO AccountName(AccName,FavFood)VALUES (?,?)");
-            ps.setString(1, name);
-            ps.setString(2, fave);
-            ps.executeUpdate();
-        } catch (Exception exception) {
-            System.out.println("Database error: " + exception.getMessage());
-        }
-    }
-    private static void Delete(){
-        try{
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("What account name do you want to delete?:-");
-            String x = scanner.nextLine();
-            PreparedStatement ps = db.prepareStatement("DELETE FROM AccountName WHERE main.AccountName.FavFood= ?");
-            ps.setString(1, x);
-            ps.executeUpdate();
-        }catch (Exception exception){
-            System.out.println("Database error: "+ exception.getMessage());
-        }
-    }
-    private static void Update(){
-        try{
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter new name:");
-            String newName = scanner.nextLine();
-            System.out.println("enter new food:");
-            String newfood = scanner.nextLine();
-            System.out.println("Enter target name:");
-            String target = scanner.nextLine();
-            PreparedStatement ps = db.prepareStatement("UPDATE main.AccountName SET main.AccountName.AccName = ?, main.AccountName.FavFood = ? WHERE main.AccountName.AccName=?");
-            ps.setString(1, newName);
-            ps.setString(2,newfood);
-            ps.setString(3, target);
-
-        }catch(Exception exception){
-            System.out.println("Database error: "+ exception.getMessage());
-        }
-    }
-
 }
+    //public static void Select(){
+       // try{
+           // PreparedStatement ps = db.prepareStatement("Select AccName, FavFood from AccountName");
+           // ResultSet results = ps.executeQuery();
+           // while (results.next()){
+           //     String AccName = results.getString(1);
+           //     String Food = results.getString(2);
+           //     System.out.println("Username: " + AccName +" Favourite Food: " + Food);
+           // }
+      //  }catch (Exception exception){
+       //     System.out.println("Database error: " + exception.getMessage());
+     //   }
+   // }
+    //public static void Add(){
+        //try{
+            //Scanner scanner = new Scanner(System.in);
+           // String name = scanner.nextLine();
+           // String fave = scanner.nextLine();
+           // PreparedStatement ps = db.prepareStatement("INSERT INTO AccountName(AccName,FavFood)VALUES (?,?)");
+           // ps.setString(1, name);
+           // ps.setString(2, fave);
+            //ps.executeUpdate();
+        //} catch (Exception exception) {
+            //System.out.println("Database error: " + exception.getMessage());
+       // }
+  //  }
+   // private static void Delete(){
+       // try{
+         //   Scanner scanner = new Scanner(System.in);
+        //    System.out.println("What account name do you want to delete?:-");
+        //    String x = scanner.nextLine();
+        //    PreparedStatement ps = db.prepareStatement("DELETE FROM AccountName WHERE main.AccountName.FavFood= ?");
+       //     ps.setString(1, x);
+       //     ps.executeUpdate();
+      //  }catch (Exception exception){
+       //     System.out.println("Database error: "+ exception.getMessage());
+      //  }
+   // }
+  //  private static void Update(){
+       // try{
+          //  Scanner scanner = new Scanner(System.in);
+         //   System.out.println("Enter new name:");
+          //  String newName = scanner.nextLine();
+          //  System.out.println("enter new food:");
+          //  String newfood = scanner.nextLine();
+          //  System.out.println("Enter target name:");
+          //  String target = scanner.nextLine();
+          //  PreparedStatement ps = db.prepareStatement("UPDATE main.AccountName SET main.AccountName.AccName = ?, main.AccountName.FavFood = ? WHERE main.AccountName.AccName=?");
+          //  ps.setString(1, newName);
+          //  ps.setString(2,newfood);
+         //   ps.setString(3, target);
+
+       // }catch(Exception exception){
+        //    System.out.println("Database error: "+ exception.getMessage());
+      //  }
+  //  }
+
+//}
