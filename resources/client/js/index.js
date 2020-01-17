6ae17fd51c7a564982a60881a964d6c21b893d22
87436 <87436@farnborough.ac.uk>
2020-01-17 14:30:48 +0000

    after xmas

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -1,10 +1,73 @@
 function pageLoad() {
 
-    let now = new Date();
-    document.getElementById("content").innerHTML = '<div style ="text-align:center;">' + '<h1>Welcome</h1>' +
-        '<div style = "font-style:italic;">' + 'Generated at ' + now.toLocaleTimeString()
-        + '</div>' + '</div>';
+    let leeR = `<table>` +
+        `<tr>` +
+        `<th>Id</th>` +
+        `<th>Name</th>` +
+        `<th>Image</th>` +
+        `<th>Location</th>` +
+        `<th>price</th>` +
+        `<th class="Last">Options</th>` +
+        `</tr>`;
 
-
-
+    fetch('/Restaurant/listRest', {method: 'get'}
+    ).then(response => response.json()
+    ).then(restaurants => {
+        for (let restaurant of restaurants) {
+            leeR += '<tr>' +
+                `<td>${restaurant.RestID}</td>` +
+                `<td>${restaurant.Rname}</td>` +
+                `<td>${postcode}</td>` +
+                `<td class= "last">` +
+                `<button class= "editButton" data-id = "${Restaurant.RestID}">EDIT </button>` +
+                `<button class="deleteButton" data-id="${Restaurant.RestID}">Delete</button>` +
+                `</td>` +
+                `</tr>`;
+        }
+        leeR += `</table>`;
+        document.getElementById("ListRest").innerHTML = leeR;
+        let editButtons = document.getElementsByClassName("editButton");
+        for (let button of editButtons) {
+            button.addEventListener("click", editRest);
+        }
+        let deleteButtons = document.getElementsByClassName("deleteButton");
+        for (let button of deleteButtons) {
+            button.addEventListener("click", deleteRest);
+        }
+    });
+    document.getElementById("saveButton").addEventListener("click", saveEditRest);
+    document.getElementById("cancelButton").addEventListener("click", cancelEditRest);
 }
+function editRest(event){
+    const id = event.target.getAttribute("data-id");
+    if (id === null){
+        document.getElementById("EditHeading").innerHTML = 'add new restaurant';
+        document.getElementById("RestID").value='';
+        document.getElementById("Rname").value='';
+        document.getElementById("postcode").value='';
+
+        document.getElementById("lastDiv").style.display = 'none';
+        document.getElementById("editDiv").style.display = 'block';
+
+    }else{
+        fetch('/Restaurant/get/'+ RestID, {method:'get'}
+        ).then(response => response.json()
+        ).then(restaurant => {
+            if(restaurant.hasOwnProperty('error')){
+                alert(restaurant.error);
+            }else{
+                document.getElementById("EditHeading").innerHTML = 'Editing'+restaurant.name +':';
+                document.getElementById("RestID").value=restaurant.name;
+                document.getElementById("Rname").value=restaurant.id;
+                document.getElementById("postcode").value=restaurant.post;
+
+                document.getElementById("lastDiv").style.display = 'none';
+                document.getElementById("editDiv").style.display = 'block';
+            }
+        }
+        )
+    }
+}
+
+
+
